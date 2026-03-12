d457e00eb868fc63348b8a5891a332fb1e02fc97
roodrygooferreira <roodrygoo2015@gmail.com>
2026-03-11 21:13:35 -0300

    Delete atividade1 directory

--- atividade1/usuario.sql
+++
@@ -1,7 +0,0 @@
-use biblioteca_pessoal_1s2026;
-create table usuario(
-id_usuario int primary key auto_increment,
-nome varchar(255) not null,
-email varchar(128) not null unique,
-data_cadastro date default(current_date) not null
-);
