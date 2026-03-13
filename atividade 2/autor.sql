f7466148c56153075ca6618a882a2c5a17ac0cc4
roodrygooferreira <roodrygoo2015@gmail.com>
2026-03-13 10:37:05 -0300

    Update autor.sql

--- atividade 2/autor.sql
+++ atividade 2/autor.sql
@@ -3,7 +3,7 @@ create table autor(
 id_autor int primary key auto_increment,
 nome varchar(128) not null,
 ano_nascimento year not null,
-ano_morte year not null,
+ano_morte year null,
 apresentacao text not null,
 data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
