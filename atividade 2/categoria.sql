0d5bad349dfadf13bb05233e0d3de3845780186e
roodrygooferreira <roodrygoo2015@gmail.com>
2026-03-13 10:58:34 -0300

    Update categoria.sql

--- atividade 2/categoria.sql
+++ atividade 2/categoria.sql
@@ -1,6 +1,6 @@
 CREATE TABLE categoria(
     id_categoria int primary key auto_increment,
-    nome varchar(128) not null,
+    nome varchar(128) not null unique,
     descricao text not null,
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
